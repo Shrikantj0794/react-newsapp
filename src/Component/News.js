f6b211f35933b6323834cb21c8a69c17d2286372
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-07 00:05:23 +0530

    Looping through an array

--- src/Component/News.js
+++ src/Component/News.js
@@ -45,18 +45,13 @@ constructor(){
         <div className="container my-3 ">
             <h2>Todays News - Top Headlines</h2>
             <div className="row">
-                <div className="col-md-4">
-                <NewsItem title='myTitle' description='myDesc' ImgUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-                </div>
-                <div className="col-md-4">
-                <NewsItem title='myTitle' description='myDesc'/>
-                </div>
-                <div className="col-md-4">
-                <NewsItem title='myTitle' description='myDesc'/>
-                </div>
+            {this.state.articles.map((element)=>{
+              return <div className="col-md-4" key={element.url} >
+              <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} ImgUrl={element.urlToImage} newsUrl={element.url}/>
+              </div>
+            })}
             </div>
         </div>
-        
       </div>
     )
   }
--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, ImgUrl}= this.props;
+    let {title, description, ImgUrl, newsUrl}= this.props;
     return (
       <div>
         <div className="card" style={{width: "20rem"}}>
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary btn-sm">Read More</a>
+            <a href={newsUrl} className="btn btn-primary btn-sm">Read More</a>
         </div>
         </div>
         </div>
