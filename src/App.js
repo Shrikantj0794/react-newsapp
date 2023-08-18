8d44fb5862e87607c08f758bb24d29b4ccd7d037
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-18 16:12:59 +0530

    Fetching News category wise in NewsApp

--- src/App.js
+++ src/App.js
@@ -15,13 +15,14 @@ export default class App extends Component {
         <Router>
         <NavBar/>
           <Routes>
-            <Route path="/"><News pageSize={8} country="in" category="general"/></Route>
-            <Route path="/business"><News pageSize={8} country="in" category="business"/></Route>
-            <Route path="/entertainment"><News pageSize={8} country="in" category="entertainment"/></Route>
-            <Route path="/health"><News pageSize={8} country="in" category="health"/></Route>
-            <Route path="/science"><News pageSize={8} country="in" category="science"/></Route>
-            <Route path="/sports"><News pageSize={8} country="in" category="sports"/></Route>
-            <Route path="/technology"><News pageSize={8} country="in" category="technology"/></Route>
+            <Route exact path="/" element={<News key="general" pageSize={8} country="in" category="general"/>} />
+            <Route exact path="/business" element={<News key="business" pageSize={8} country="in" category="business"/>} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={8} country="in" category="entertainment"/>} />
+            <Route exact path="/general" element={<News key="general" pageSize={8} country="in" category="general"/>} />
+            <Route exact path="/health" element={<News key="health" pageSize={8} country="in" category="health"/>} />
+            <Route exact path="/science" element={<News key="science" pageSize={8} country="in" category="science"/>} />
+            <Route exact path="/sports" element={<News key="sports" pageSize={8} country="in" category="sports"/>} />
+            <Route exact path="/technology" element={<News key="technology" pageSize={8} country="in" category="technology"/>} />
           </Routes>
         </Router>
       </>
