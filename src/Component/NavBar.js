137b52576ac0e2ae568925e64e3ba73ebf2937ef
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-18 16:30:24 +0530

    News showing category wise

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -6,7 +6,7 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
                         <Link className="navbar-brand"to="/">NewsApp</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,13 +17,21 @@ export class NavBar extends Component {
                             <li className="nav-item">
                             <Link className="nav-link" aria-current="page"to="/">Home</Link>
                             </li>
-                           <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+
+                            <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Category
+                            </a>
+                            <ul class="dropdown-menu">
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                            </ul>
+                            </li>
                             </ul>
                         </div>
                     </div>
